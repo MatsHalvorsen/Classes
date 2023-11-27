6395f622ca661ef103c4c4f46df98ff808adc911
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-11-27 10:12:18 +0100

    first commit

---
+++ Classes/Program.cs
@@ -0,0 +1,3 @@
+﻿var person = new { Name = "Mats", Age = 28, Hobby = "Training", Job = "Developer" };
+
+Console.WriteLine($"Mitt navn er {person.Name}, jeg er {person.Age} år gammel. Jeg jobber som {person.Job}, å på fritiden liker jeg {person.Hobby}.");
